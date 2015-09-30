db57851ac6b6fa736bb3efe1d88664600a22fcc5
David Swaintek <dswaintek@gmail.com>
2015-09-30 13:23:16 -0700

    Add in error correction

--- game.js
+++ game.js
@@ -1,22 +1,22 @@
-var question1 = prompt("Is Seattle a great music town?");
-if (question1.toUpperCase() === "YES") {
-	alert("Correct!  You rock.");
+var question1 = prompt('Is Seattle a great music town?');
+if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+	alert('Correct!  You rock.');
 } else {
-	alert("Incorrect. Seattle music rocks.");
+	alert('Incorrect. Seattle music rocks.');
 }
 
-var question2 = prompt("Is Kurt Cobain dead?");
-if (question2.toUpperCase() === "YES") {
-	alert("Correct. Dead as a doornail.");
+var question2 = prompt('Is Kurt Cobain dead?');
+if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === "Y") {
+	alert('Correct. Dead as a doornail.');
 } else {
-	alert("Maybe not in spirit, but he is definitely in body!");
+	alert('Maybe not in spirit, but he is definitely in body!');
 }
 
-var question3 = prompt("Does Eddie Vedder love flannel?");
-if (question3.toUpperCase() === "YES") {
-	alert("Yes. So comfortable.");
+var question3 = prompt('Does Eddie Vedder love flannel?');
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === "Y") {
+	alert('Yes. So comfortable.');
 } else {
-	alert("Incorrect.  Flannel is mandatory in Seattle.")
+	alert('Incorrect.  Flannel is mandatory in Seattle.')
 }
 
 console.log('Question 1 response: ' + question1);
