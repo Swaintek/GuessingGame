f934da287fd46ce03c2305d3cdbe6fa50a522f5e
Apple <apple@Apples-MacBook-Pro-3.local>
2015-10-01 13:18:50 -0700

    Added functions to GuessingGame

--- game.js
+++ game.js
@@ -3,29 +3,39 @@ var correctAnswers = 0;
 var userName = prompt('Please enter your name.');
 alert('Welcome ' + userName + ', let\'s play a game!');
 
+function ques1 () {
 var question1 = prompt('Is Seattle a great music town?');
-if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
-	alert('Correct!  You rock.');
-	correctAnswers ++;
-} else {
-	alert('Incorrect. Seattle music rocks.');
+	if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+		alert('Correct!  You rock.');
+		correctAnswers ++;
+	} else {
+		alert('Incorrect. Seattle music rocks.');
+	}
 }
 
-var question2 = prompt('Is Kurt Cobain dead?');
-if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === "Y") {
-	alert('Correct. Dead as a doornail.');
-	correctAnswers ++;
-} else {
-	alert('Maybe not in spirit, but he is definitely in body!');
+function ques2 () {
+	var question2 = prompt('Is Kurt Cobain dead?');
+	if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === "Y") {
+		alert('Correct. Dead as a doornail.');
+		correctAnswers ++;
+	} else {
+		alert('Maybe not in spirit, but he is definitely in body!');
+	}
 }
 
-var question3 = prompt('Does Eddie Vedder love flannel?');
-if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === "Y") {
-	alert('Yes. So comfortable.');
-	correctAnswers ++;
-} else {
-	alert('Incorrect.  Flannel is mandatory in Seattle.');
+function ques3 () {
+	var question3 = prompt('Does Eddie Vedder love flannel?');
+	if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === "Y") {
+		alert('Yes. So comfortable.');
+		correctAnswers ++;
+	} else {
+		alert('Incorrect.  Flannel is mandatory in Seattle.');
+	}
 }
+
+ques1();
+ques2();
+ques3();
 
 alert('You got ' + correctAnswers + ' out of 3 answers correct, ' + userName + ', thanks for playing!');
 
