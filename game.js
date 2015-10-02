34c817a6febc0ce8fe6e7b017beccd57b1394d54
Natalie Chow <cychownat@gmail.com>
2015-10-02 13:02:21 -0700

    Added images to correct answers

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ var answers = ['YES', 'Y', 'NO', 'N'];
 function ques1 () {
 var question1 = prompt(questions[0]);
 	if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1]) {
-		one.innerHTML = 'Correct!  You rock.';
+		one.innerHTML = 'Correct!  You rock.' + '<br /><img src="images/bumbershoot.jpg" height="200" width="200" />';
 		correctAnswers ++;
 	} else {
 		one.innerHTML = 'Incorrect. Seattle music rocks.';
@@ -21,7 +21,7 @@ var question1 = prompt(questions[0]);
 function ques2 () {
 	var question2 = prompt(questions[1]);
 	if (question2.toUpperCase() === answers[0] || question2.toUpperCase() === answers[1]) {
-		two.innerHTML = 'Correct. Dead as a doornail.';
+		two.innerHTML = 'Correct. Dead as a doornail.' + '<br /><img src="images/kurtcobain.jpg" height="200" width="200" />';
 		correctAnswers ++;
 	} else {
 		two.innerHTML = 'Maybe not in spirit, but he is definitely in body!';
@@ -31,7 +31,7 @@ function ques2 () {
 function ques3 () {
 	var question3 = prompt(questions[2]);
 	if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[1]) {
-		three.innerHTML = 'Yes. So comfortable.';
+		three.innerHTML = 'Yes. So comfortable.' + '<br /><img src="images/flannel.jpg" height="200" width="200" />';
 		correctAnswers ++;
 	} else {
 		three.innerHTML = 'Incorrect.  Flannel is mandatory in Seattle.';
